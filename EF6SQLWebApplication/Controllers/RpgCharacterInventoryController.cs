f739779f48a4495551c59c76e3e78052e5641bcc
odarmokhval <olga.darmokhval@gmail.com>
2024-04-19 13:41:23 +0300

    add application

---
+++ EF6SQLWebApplication/Controllers/RpgCharacterInventoryController.cs
@@ -0,0 +1,49 @@
+﻿using EF6SQLWebApplication.Data;
+using EF6SQLWebApplication.Interfaces;
+using EF6SQLWebApplication.Intergaces;
+using EF6SQLWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EF6SQLWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RpgCharacterInventoryController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly IUnitOfWork _uow;
+
+        public RpgCharacterInventoryController(DataContext context, IUnitOfWork uow)
+        {
+            _context = context;
+            _uow = uow;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<RpgCharacterInventory>> AddItemToInventory(RpgCharacterInventory item)
+        {
+            _uow.RpgCharacterInventoryRepository.AddItemToInventory(item);
+            await _uow.SaveAsync();
+
+            return Ok(item);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<RpgCharacterInventory>>> GetAllItems()
+        {
+            var items = await _uow.RpgCharacterInventoryRepository.GetAllItems();
+            return Ok(items);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RpgCharacterInventory>> GetItem(int id)
+        {
+            var item = _uow.RpgCharacterInventoryRepository.GetItem(id);
+            if (item == null)
+            {
+                return NotFound("Item not found");
+            }
+            return Ok(item);
+        }
+    }
+}
---
+++ EF6SQLWebApplication/Interfaces/IRpgCharacterInventoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using EF6SQLWebApplication.Models;
+
+namespace EF6SQLWebApplication.Intergaces
+{
+    public interface IRpgCharacterInventoryRepository
+    {
+        void AddItemToInventory(RpgCharacterInventory item);
+        Task<IEnumerable<RpgCharacterInventory>> GetAllItems();
+        Task<RpgCharacterInventory> GetItem(int id);
+    }
+}
